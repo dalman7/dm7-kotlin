9016e0d92f2d6572066a70d3fa8a820d28b94df1
doubleseven <double.seven@navercorp.com>
2018-07-27 16:06:55 +0900

    init spring boot2 kotlin based project

---
+++ src/main/kotlin/kr/co/dm7/blackpink/Dm7KotlinApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.dm7.blackpink
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Dm7KotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<Dm7KotlinApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/dm7/blackpink/Dm7KotlinApplicationTests.kt
@@ -0,0 +1,16 @@
+package kr.co.dm7.blackpink
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Dm7KotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
