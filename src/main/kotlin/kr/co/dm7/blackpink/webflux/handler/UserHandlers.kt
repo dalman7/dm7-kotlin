5181614f7a3721391ed1523eaf280726c9a43232
doubleseven <double.seven@navercorp.com>
2018-08-04 15:52:45 +0900

    add webflux handler and route

--- src/main/kotlin/kr/co/dm7/blackpink/webflux/handler/UserHandlers.kt
+++ src/main/kotlin/kr/co/dm7/blackpink/webflux/handler/UserHandlers.kt
@@ -5,41 +5,104 @@ import kr.co.dm7.blackpink.repository.mongo.DomainRepository
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.*
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
 
+/**
+ * webflux 로 handler 구현 샘플
+ * dm7 프로젝트의 DomainController 부분을 webflux 기반으로 구현했다
+ *
+ * @author doubleseven
+ * @version 1.0
+ */
 @Component
 class DomainHandler(
         val domainRepository: DomainRepository) {
 
-    fun getAll(request: ServerRequest) = ServerResponse
-            .ok()
+    /**
+     * Domain 전체 정보를 얻는다.
+     */
+    fun getAll(request: ServerRequest) = ok()
             .body(
                     domainRepository.findAll(),
                     Domain::class.java
             )
 
+    /**
+     * Domain 정보를 추가한다.
+     */
     fun addAll(request: ServerRequest): Mono<ServerResponse> =
             request.bodyToMono(Domain::class.java)
                     .flatMap {
                         domainRepository.save(it).toMono()
                     }
                     .flatMap {
-                        ServerResponse.ok().body(it.toMono(), Domain::class.java)
+                        ok().body(it.toMono(), Domain::class.java)
                     }
                     .onErrorResume {
-                        ServerResponse.badRequest().body(
+                        badRequest().body(
                                 ErrorResponse(message = it.message ?: "error").toMono(), ErrorResponse::class.java)
                     }
 
+    /**
+     * Domain 정보 한건을 얻는다
+     */
+    fun getOneById(request: ServerRequest) = ok()
+            .body(
+                    domainRepository.findById(request.pathVariable("id")),
+                    Domain::class.java
+            )
 
-    fun getOneById(request: ServerRequest) =
-            ServerResponse
-                    .ok()
-                    .body(
-                            domainRepository.findById(request.pathVariable("id")),
-                            Domain::class.java
-                    )
+    /**
+     * 이름으로 도메인 객체 찾기
+     */
+    fun getOneByName(request: ServerRequest) = ok()
+            .body(
+                    domainRepository.findFirstByName(request.pathVariable("name")),
+                    Domain::class.java
+            )
+
+    /**
+     * 이름으로 도메인 객체 찾기 ( mongo json query 이용 )
+     */
+    fun getOneByCustomName(request: ServerRequest) = ok()
+            .body(
+                    domainRepository.findCustomByName(request.pathVariable("name")),
+                    Domain::class.java
+            )
+
+    /**
+     * 이름과 노출여부 정보로 도메인 객체 찾기
+     */
+    fun getOneByNameAndDisplay(request: ServerRequest) = ok()
+            .body(
+                    domainRepository.findByNameAndDisplay(request.pathVariable("name"), request.pathVariable("display").toBoolean()),
+                    Domain::class.java
+            )
+    /**
+     * 도메인의 상태 계속 호출할때마다 바꿔준다. ( display :true <-> false )
+     */
+    fun changeDisplayAndGet(request: ServerRequest): Mono<ServerResponse> {
+        val name = request.pathVariable("name")
+        domainRepository.findFirstByName(name)
+                .subscribe(
+                        { value -> domainRepository.updateDisplay(value.name, !value.display) },
+                        { error -> error.printStackTrace() },
+                        { println("completed without a value") }
+                )
+        return ok()
+                .body(
+                        domainRepository.findFirstByName(name),
+                        Domain::class.java
+                )
+    }
+
+    fun getByRegExpName(request: ServerRequest) =
+            ok().body(
+                    domainRepository.findCustomByRegExDomain(request.pathVariable("name")),
+                    Domain::class.java
+            )
 
     data class ErrorResponse(val message: String)
 }
--- src/main/kotlin/kr/co/dm7/blackpink/webflux/routing/RoutingConfig.kt
+++ src/main/kotlin/kr/co/dm7/blackpink/webflux/routing/RoutingConfig.kt
@@ -6,16 +6,36 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.router
 
+/**
+ * webflux 로 route 구현 샘플
+ * dm7 프로젝트의 Controller 부분을 webflux 기반으로 구현했다
+ *
+ * @author doubleseven
+ * @version 1.0
+ */
 @Configuration
 class RoutingConfig {
 
     @Bean
     fun domainRouter(domainHandler: DomainHandler) = router {
         accept(MediaType.APPLICATION_JSON).nest {
-            GET("/domains", domainHandler::getAll)
+            GET("/domains", domainHandler::getAll)                                  // [GET] http://localhost:8080/domains
             "/domain".nest {
-                GET("{id}", domainHandler::getOneById)
-                PUT("/", domainHandler::addAll)
+                GET("/{id}", domainHandler::getOneById)                             // [GET] http://localhost:8080/domain/{id}
+                PUT("/", domainHandler::addAll)                                     // [GET] http://localhost:8080/domain    request body json : {"name": "칠칠월드2","url": "http://okay.com","address": {"postCode": "777-777","firstAddress": "경기도 성남시","secondAddress": "오케이 아파트 가동 111호"}, "display": true}
+                "/name".nest {
+                    GET("/{name}", domainHandler::getOneByName)                     // [GET] http://localhost:8080/domain/name/만동홈페이지
+                    "/regexp".nest {
+                        GET("/{name}", domainHandler::getByRegExpName)              // [GET] http://localhost:8080/domain/name/regexp/만동
+                    }
+                    "/{name}".nest {
+                        GET("/changeDisplay", domainHandler::changeDisplayAndGet)   // [GET] http://localhost:8080/domain/name/만동홈페이지/changeDisplay
+                        GET("/{display}", domainHandler::getOneByNameAndDisplay)    // [GET] http://localhost:8080/domain/name/만동홈페이지/false or false
+                    }
+                }
+                "/name2".nest {
+                    GET("/{name}", domainHandler::getOneByCustomName)                // [GET] http://localhost:8080/domain/name2/만동홈페이지
+                }
             }
         }
     }
